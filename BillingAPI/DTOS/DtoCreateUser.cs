00aff53474eb201d91f87663a718386ad171de6f
Rafael Eduardo Melo Rodriguez <rafaeleduardomelorodriguez@gmail.com>
2023-11-27 18:21:34 -0500

    feat: create user

--- BillingAPI/DTOS/DtoCreateUser.cs
+++ BillingAPI/DTOS/DtoCreateUser.cs
@@ -1,23 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BillingAPI.DTOS
+﻿namespace BillingAPI.DTOS
 {
-    public class DtoUser
+    public class DtoCreateUser
     {
-        
-        public int id { get; set; }
         public string userName { get; set; }
         public string password { get; set; }
         public string email { get; set; }
         public string name { get; set; }
         public string lastName { get; set; }
-        public int addressId { get; set; }
-        public string phone { get; set; } 
-        public string ipAddress { get; set; }
-        public string macAddress { get; set; }
-        public DateTime lastLogin { get; set; }
+        public string phone { get; set; }
         public bool userType { get; set; }
-        public bool status { get; set; }
 
         //Address 
         public string address { get; set; }
@@ -26,6 +17,5 @@ namespace BillingAPI.DTOS
         public string country { get; set; }
         public string postalCode { get; set; }
 
-        public string ErrorMessage { get; set; }
     }
 }
